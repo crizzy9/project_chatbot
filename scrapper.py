fc6b0b7db7973318fd92346e4bfe27a89365d1e5
taylor <trgosselin14@gmail.com>
2017-11-20 20:42:02 -0500

    added data

--- scrapper.py
+++ scrapper.py
@@ -5,6 +5,11 @@ from lxml import html
 page = requests.get('https://azure.microsoft.com/en-us/support/faq/')
 data = html.fromstring(page.content)
 
+counter = 0
 for entry in data.xpath('//li[@class="faq-entry"]'):
     question = entry.xpath('button/text()')[0]
     answer = entry.xpath('div/p/text()')
+    counter += 1
+    with open(r'C:\Users\Taylor\Documents\Webscapper\Docs\qa{}.txt'.format(counter),'w') as f:
+        f.write("".join([question, '\n', " ".join(answer)]))
+        f.close
